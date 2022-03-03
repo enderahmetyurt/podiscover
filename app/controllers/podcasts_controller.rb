cefaf1284d53c8db6fcdddb2d5dc4a54759fe2d9
Ender Ahmet Yurt <enderyurt@gmail.com>
2022-03-03 22:18:43 +0300

    Hide current user podcasts

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -6,7 +6,7 @@ class PodcastsController < ApplicationController
 
   # GET /podcasts or /podcasts.json
   def index
-    @pagy, @podcasts = pagy_countless(Podcast.all.order(:name))
+    @pagy, @podcasts = pagy_countless(Podcast.where.not(user_id: current_user.id).order(:name))
   end
 
   # GET /podcasts/1 or /podcasts/1.json

d8493900a5dbb3d6d8fdc61b736da76ba860f7f3
Ender Ahmet Yurt <enderyurt@gmail.com>
2022-03-04 19:59:46 +0300

    Change color current link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   include Pagy::Frontend
+
+  def current_class?(current_path)
+    return 'text-red-600' if request.path == current_path
+    ''
+  end  
 end

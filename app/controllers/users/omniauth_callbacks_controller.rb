07358b443a1615a2013f908b3034cb123410bc49
Ender Ahmet Yurt <enderyurt@gmail.com>
2022-03-29 19:44:14 +0300

    Add warning message for unauthorized spotify users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,11 +5,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def spotify
     @user = User.from_omniauth(request.env['omniauth.auth'])
-
+    
     sign_in_and_redirect @user
   end
 
   def failure
+    flash[:warn] = '🥺 This is a private beta project. Access to your Spotify account must be granted to continue.'
     redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def index
     users = User.where.not(id: current_user.id).to_a
-    @users = users.sort_by!{|user| user.podcasts.count}.reverse
+    @users = users.sort_by!{ |user| user.podcasts.count }.reverse
   end
 
   def show

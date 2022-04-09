e4e3cb502583fa8f8d52480d52c32cbdb24d560b
Ender Ahmet Yurt <enderyurt@gmail.com>
2022-04-09 14:55:58 +0300

    Order by podcast users count

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,7 +4,8 @@ class SubscriptionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @subscriptions = current_user.feed.uniq { |s| s.podcast_id }
+    subscriptions = current_user.feed.uniq { |s| s.podcast_id }
+    @subscriptions = subscriptions.sort_by{|s| s.podcast.users.count}.reverse
   end
 
   def show

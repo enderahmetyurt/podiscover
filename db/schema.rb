1eb825cbf4f75198dab59270357f57c27ec4c64b
Ender Ahmet Yurt <enderyurt@gmail.com>
2022-04-10 17:24:54 +0300

    Remove unnecessary migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_08_170416) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_09_112905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,7 +79,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_170416) do
     t.string "display_name"
     t.string "external_url"
     t.string "image_url"
-    t.string "spotify_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

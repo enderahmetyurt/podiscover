7429421e33598c105fdc444bc3fdf8c497315035
Ender Ahmet Yurt <enderyurt@gmail.com>
2022-04-27 15:04:59 +0300

    Change scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,5 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'],
-           scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'], scope: 'user-read-email user-library-read user-read-playback-position'
 end

14d2dcb4bf548fcb5e9a6cca15fab736f98566ac
Ender Ahmet Yurt <enderyurt@gmail.com>
2022-05-18 08:41:13 +0300

    Add categories section to aside

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,10 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @categories = Category.all
+  end
+
   def show
     @category = Category.find(params[:id])
     @podcasts = @category.podcasts
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,19 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'category/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   authenticate :user, lambda { |u| u.is_admin? } do
     mount Sidekiq::Web => '/sidekiq'
-  end  
+  end
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :podcasts, only: %i[index show] do
     resources :episodes, only: %i[show]
   end
-  
+
   resources :dashboard, only: [:index]
-  
+
   resources :users, only: %i[index show] do
     member do
       get :following, :followers
@@ -25,10 +26,10 @@ Rails.application.routes.draw do
       get :tigers
     end
   end
-  
+
   resources :relationships, only: %i[create destroy]
   resources :subscriptions, only: %i[index show]
-  resources :categories, only: %i[show]
+  resources :categories, only: %i[index show]
 
   root 'home#index'
 end

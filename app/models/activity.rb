89521b9bb15ab9cd873764cc0284ed5ae49fa9ea
Çağrı Özkan <ccozkan@tutanota.com>
2022-10-12 00:09:51 +0300

    Check whether user_owned_activity when validating

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,5 +2,12 @@
 
 class Activity < ApplicationRecord
   belongs_to :user, optional: true
+  validates_presence_of :user, if: :user_owned_activity?
+
   delegated_type :activatable, types: %w[ Listen Commentable Likeable Broadcastable]
+
+  def user_owned_activity?
+    return false unless broadcastable? && broadcastable.new_version_has_released?
+    true
+  end
 end

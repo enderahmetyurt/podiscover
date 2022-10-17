8efac567eb7eb75aa39663ef83f1b2114d280013
Ender Ahmet Yurt <enderyurt@gmail.com>
2022-10-17 20:41:22 +0300

    Apply PR review

--- app/models/followable.rb
+++ app/models/followable.rb
@@ -1,3 +1,7 @@
 class Followable < ApplicationRecord
+  AVAILABLE_ACTIONS = %w(followed unfollowed).freeze
+
+  validates :action, inclusion: { in: AVAILABLE_ACTIONS }
+
   belongs_to :followed, class_name: 'User'
 end
--- spec/factories/followables.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :followable do
-    follower_id { 1 }
-    followed_id { 1 }
-    action { "MyString" }
-  end
-end

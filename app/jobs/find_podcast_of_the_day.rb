b59e30e20ad7eac6862ef093a393a95db52354e1
Ender Ahmet Yurt <enderyurt@gmail.com>
2022-11-17 08:57:25 +0300

    Use body

--- app/jobs/find_podcast_of_the_day.rb
+++ app/jobs/find_podcast_of_the_day.rb
@@ -19,8 +19,9 @@ class FindPodcastOfTheDay
   end
 
   def send_zapier(podcast)
-    uri = URI("https://hooks.zapier.com/hooks/catch/758406/bpfbsua/?podcast=#{podcast.external_url}")
-    res = Net::HTTP.post_form(uri, {})
+    uri = URI("https://hooks.zapier.com/hooks/catch/758406/bpfbsua/")
+    body = {name: podcast.name, external_url: podcast.external_url}
+    res = Net::HTTP.post_form(uri, body)
     Rails.logger.info(res.body) if res.is_a?(Net::HTTPSuccess)
   end
 end

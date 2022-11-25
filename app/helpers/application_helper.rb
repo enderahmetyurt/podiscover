7d97e5d5ee54e511834f7acc4c2e100f23824c46
Furkan Bayram <furkanbayram@msn.com>
2022-11-26 00:17:02 +0300

    new font added. layout(header/footer) changed. profile, community page updated.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,8 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def current_class?(current_path)
-    return 'text-red-600' if request.path == current_path
+    return 'text-primary bg-secondary px-6 py-4 rounded-lg flex items-center space-x-2' if request.path == current_path
 
-    ''
+    'px-6 py-4 hover:bg-gray-50 rounded-lg flex items-center space-x-2'
   end
 end

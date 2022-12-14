97f06dfee76bf7a849c300e07e15df3ee202da53
Furkan Bayram <furkanbayram@msn.com>
2022-12-14 16:23:22 +0300

    sidebar button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,12 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def current_class?(current_path)
-    return 'text-primary bg-secondary px-6 py-4 rounded-lg flex items-center space-x-2' if request.path == current_path
-
-    'px-6 py-4 hover:bg-gray-50 rounded-lg flex items-center space-x-2'
+    default_classes = 'px-6 py-4 rounded-lg flex items-center space-x-2'
+    
+    if request.path == current_path
+      "text-primary bg-secondary #{default_classes}" 
+    else
+      "hover:bg-gray-50 #{default_classes}" 
+    end
   end
 end

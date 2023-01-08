093d8a6dc6218ab9a0c57bb49762eaf6d620f0ac
Ender Ahmet Yurt <enderyurt@gmail.com>
2023-01-08 16:06:36 +0300

    Change category index page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,15 @@
 class Category < ApplicationRecord
   has_many :genres, dependent: :destroy
   has_many :podcasts, through: :genres
+
+  def sample_podcasts
+    sample_podcasts = self.podcasts.sample(4)
+    result = []
+
+    Array.new(4).each_with_index do |element, index|
+      result << podcasts[index]
+    end
+
+    result
+  end
 end

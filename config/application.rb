f34c7fb07fde8ee9c211010ef73a1f694479984c
kiliczsh <muhammeddkilicc@gmail.com>
2023-04-07 18:10:57 +0300

    Revert "Add Redis, Fix callback url, sidekiq and some ui conditions"
    This reverts commit e1b39a208f26b6a7bea0d7e72da1d5001115ad14.

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,6 @@ module Spotipods
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Permission for web console
-    config.web_console.permissions = '172.30.0.1'
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

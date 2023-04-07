e1b39a208f26b6a7bea0d7e72da1d5001115ad14
kiliczsh <muhammeddkilicc@gmail.com>
2023-04-07 18:10:57 +0300

    Add Redis, Fix callback url, sidekiq and some ui conditions

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -8,10 +8,14 @@ class PodcastsController < ApplicationController
   end
 
   def show
-    @podcast = Podcast.find(params[:id])
-    @all_episodes = @podcast.episodes.sort_by { |e| e.release_date.to_date }.reverse
-    @episodes = @all_episodes.first(5)
-    @subscribers = @podcast.subscriptions.first(5)
+    if params[:id].present?
+      @podcast = Podcast.find(params[:id])
+      @all_episodes = @podcast.episodes.sort_by { |e| e.release_date.to_date }.reverse
+      @episodes = @all_episodes.first(5)
+      @subscribers = @podcast.subscriptions.first(5)
+    else
+      redirect_to root_path
+    end
   end
 
   def search

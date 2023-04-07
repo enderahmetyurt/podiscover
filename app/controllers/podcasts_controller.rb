8797a43b8036f610dcc04c9cb46840078b7beb1d
kiliczsh <muhammeddkilicc@gmail.com>
2023-04-07 18:10:57 +0300

    Revert "Add Redis, Fix callback url, sidekiq and some ui conditions"
    This reverts commit e1b39a208f26b6a7bea0d7e72da1d5001115ad14.

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -8,14 +8,10 @@ class PodcastsController < ApplicationController
   end
 
   def show
-    if params[:id].present?
-      @podcast = Podcast.find(params[:id])
-      @all_episodes = @podcast.episodes.sort_by { |e| e.release_date.to_date }.reverse
-      @episodes = @all_episodes.first(5)
-      @subscribers = @podcast.subscriptions.first(5)
-    else
-      redirect_to root_path
-    end
+    @podcast = Podcast.find(params[:id])
+    @all_episodes = @podcast.episodes.sort_by { |e| e.release_date.to_date }.reverse
+    @episodes = @all_episodes.first(5)
+    @subscribers = @podcast.subscriptions.first(5)
   end
 
   def search
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,3 @@
-Sidekiq.strict_args!(false)
-
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'], size: 4, network_timeout: 5 }
 end

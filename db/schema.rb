d4758ffa7f256c35504cf67b4d47c0a436315780
kiliczsh <muhammeddkilicc@gmail.com>
2023-07-07 17:56:17 +0300

    Enable Timescale DB extension for Railway

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_06_17_075921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "active_analytics_views_per_days", force: :cascade do |t|
     t.string "site", null: false

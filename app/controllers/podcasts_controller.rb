5da8fbaacd06528e6a90f646bc735c067e821cca
Ender Ahmet Yurt <enderyurt@gmail.com>
2023-10-26 19:33:46 +0300

    Show all subscribers count

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -11,9 +11,8 @@ class PodcastsController < ApplicationController
     redirect_to podcasts_path unless params[:id].present?
 
     @podcast = Podcast.find(params[:id])
-    @all_episodes = @podcast.episodes.sort_by { |e| e.release_date.to_date }.reverse
-    @episodes = @all_episodes.first(5)
-    @subscribers = @podcast.subscriptions.first(5)
+    all_episodes = @podcast.episodes.sort_by { |e| e.release_date.to_date }.reverse
+    @episodes = all_episodes.first(5)
   end
 
   def search
--- app/helpers/podcasts_helper.rb
+++ app/helpers/podcasts_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module PodcastsHelper
+  def first_five_subscribers(subscriptions)
+    subscriptions.first(5)
+  end
 end

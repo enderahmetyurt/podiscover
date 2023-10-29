60252cf10a9322c6746db816afbe6846b2816fd5
Ender Ahmet Yurt <enderyurt@gmail.com>
2023-10-29 18:06:34 +0300

    Start host project (#220)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_27_052136) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_27_133433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -189,6 +189,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_27_052136) do
     t.datetime "podcast_of_the_day_at"
     t.jsonb "extras", default: {}
     t.string "feed_url"
+    t.integer "owner_id"
   end
 
   create_table "relationships", force: :cascade do |t|
@@ -239,6 +240,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_27_052136) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.boolean "update_manuelly", default: false
+    t.boolean "host", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true

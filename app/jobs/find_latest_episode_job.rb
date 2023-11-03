ce2e7b30e448836e7ba01dcc425f858439e8e323
Ender Ahmet Yurt <enderyurt@gmail.com>
2023-11-03 15:55:41 +0300

    Tweak find current episodes

--- app/jobs/find_latest_episode_job.rb
+++ app/jobs/find_latest_episode_job.rb
@@ -5,15 +5,7 @@ class FindLatestEpisodeJob
   def perform(user_id)
     @user = User.find(user_id)
     @podcasts = @user.podcasts
-    new_episodes = []
-
-    @podcasts.each do |podcast|
-      podcast.episodes.each do |episode|
-        if episode.release_date.to_date == Date.current
-          new_episodes << episode
-        end
-      end
-    end
+    new_episodes = @user.podcasts.joins(:episodes).where(episodes: {release_date: Date.current})
 
     if new_episodes.any?
       UserMailer.new_episodes(@user, new_episodes).deliver_now

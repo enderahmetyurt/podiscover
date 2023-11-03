502f602b19c56c21df54628b31524d193c60051f
Ender Ahmet Yurt <enderyurt@gmail.com>
2023-11-03 20:17:29 +0300

    Change find current episode query

--- app/jobs/find_latest_episode_job.rb
+++ app/jobs/find_latest_episode_job.rb
@@ -3,9 +3,17 @@ class FindLatestEpisodeJob
   sidekiq_options queue: "default"
 
   def perform(user_id)
-    @user = User.find(user_id)
-    @podcasts = @user.podcasts
-    new_episodes = @user.podcasts.joins(:episodes).where(episodes: {release_date: Date.current})
+    user = User.find(user_id)
+    podcasts = user.podcasts
+    new_episodes = []
+
+    podcasts.each do |podcast|
+      podcast.episodes.each do |episode|
+        if episode.release_date == Date.current
+          new_episodes << episode
+        end
+      end
+    end
 
     if new_episodes.any?
       UserMailer.new_episodes(@user, new_episodes).deliver_now
